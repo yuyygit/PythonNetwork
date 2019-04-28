c73dc5abb2efec4a2a80d6f3814dea73574511c9
yuyang <1307271447@qq.com>
2019-04-28 11:19:15 +0800

    3-10UDS

---
+++ 3IPv6UnixSocket/3-10unixDomainSocket/3_10unixDomainClient.py
@@ -0,0 +1,36 @@
+# encoding: utf-8
+import socket
+import sys
+
+SERVER_PATH = "/tmp/python_unix_socket_server"
+
+def run_unix_domain_socket_client():
+    """Run a Unix domain socket client"""
+    sock = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
+
+    #Connect the socket to the path where the server is listening
+    server_address = SERVER_PATH
+    print "connect to %s " % server_address
+    try:
+        sock.connect(server_address)
+    except socket.error as msg:
+        print >>sys.stderr, msg
+        sys.exit(1)
+
+    try:
+        message = "This is the message. This will be echoed back!"
+        print "Sending [%s]" % message
+        sock.sendall(message)
+        amount_received = 0
+        amount_expected = len(message)
+
+        while amount_received < amount_expected:
+            data = sock.recv(16)
+            amount_received += len(data)
+            print >>sys.stderr, "Received [%s]" % data
+    finally:
+        print "Closing client"
+        sock.close()
+if __name__ == '__main__':
+    run_unix_domain_socket_client()
+
